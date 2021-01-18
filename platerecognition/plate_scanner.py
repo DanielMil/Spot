8285e51ed4ca8e0ffd1aec9a564b15a86f271576
Danyal Mahmood <dmahmood@mail.uoguelph.ca>
2021-01-18 16:02:20 -0500

    finish plate scanner

---
+++ platerecognition/plate_scanner.py
@@ -0,0 +1,33 @@
+import cv2
+import pytesseract
+
+# import open source neural net
+classifier = cv2.CascadeClassifier("resources/haarcascade_russian_plate_number.xml")
+
+# read in image of liscence plate and convert to black and white
+pic = cv2.imread("testdataset/Cars0.png") # (TODO) covert to webcam after
+picGray = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
+
+# use neural net to detect plate
+plate = classifier.detectMultiScale(picGray, 1.1, 5)
+
+# loop through co ordinates of plate and crop image to optimal parameters of plate
+for x,y,w,h in plate:
+        croppedPlate = picGray[y+15:y+h-5, x+15:x+w-5]
+
+
+# if car plate not found return image
+try:
+    croppedPlate # does a exist in the current namespace
+except NameError:
+    print("Plate not found.")
+    exit()
+
+# double the size of the image
+bigImage = cv2.resize(croppedPlate, (int(croppedPlate.shape[1] * 2), int(croppedPlate.shape[0] * 2)), 3)
+# use median blur technique to reduce noise in the image
+finalImage = cv2.medianBlur(bigImage, 5)
+
+# display detected licence plate
+plateNumber = pytesseract.image_to_string(finalImage, 'eng', '-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 8')
+print(plateNumber)
