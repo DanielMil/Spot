eac3d67b7a53af4b3f733457b7a88cf3afcf8ebf
DanielMil <dmil@mail.uoguelph.ca>
2021-03-17 19:18:43 -0400

    Init shell

--- init.sh
+++ init.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-. ./env.sh
+export $(grep -v '^#' .env | xargs)
 
+# Build frontend
+cd client
+npm i
+npm run build
+
+# Move build to server container
+rm -rf ../server/build_client
+mv build ../server/build_client
+
+#start containers
 docker-compose up --build -d
